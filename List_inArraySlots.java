d8a59d9acaa8254b1b5c09ae28a0f79de5136aed
Max Vaysburd <max.vays@gmail.com>
2019-03-06 12:49:09 -0500

    Copy skeleton

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,7 +1,54 @@
-public class List_inArraySlots{
+/**
+  Implement a list of integer elements, including
+  both data and operations.
+ */
 
-	public List_inArraySlots(){
-	
-	}
+public class List_inArraySlots {
 
+    // declare fields here
+
+    /**
+      Construct an empty list with a small initial capacity.
+     */
+    public List_inArraySlots() {
+    }
+
+
+    /** 
+      @return the number of elements in this list
+     */
+    // public int size() {
+    // }
+
+
+     /** 
+       @return a string representation of this list,
+       in [a,b,c,] format
+      */ 
+    // public String toString() {
+    // }
+
+    
+    /** 
+      Appends @value to the end of this list.
+      
+      @return true, in keeping with conventions yet to be discussed
+     */
+     // public boolean add( int value) {
+     // }
+
+
+    /** 
+      Double the capacity of the List_inArraySlots, 
+      preserving existing data
+     */
+     // private void expand() {
+        // System.out.println( "expand... (for debugging)");
+           // /* S.O.P. rules for debugging:
+              // Working methods should be silent. But during 
+              // development, the programmer must verify that 
+              // this method is called when that is appropriate.
+              // So test using the println(), then comment it out.
+              // */
+     // }
 }
