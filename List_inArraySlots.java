d1db718207a3c0d877928a90cbb0c8585e9a773b
Max Vaysburd <max.vays@gmail.com>
2019-03-06 13:00:38 -0500

    Add size field and get method

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -7,6 +7,7 @@ public class List_inArraySlots {
 
     // declare fields here
 	int[] list;
+	int size;
 
     /**
       Construct an empty list with a small initial capacity.
@@ -19,9 +20,9 @@ public class List_inArraySlots {
     /** 
       @return the number of elements in this list
      */
-    //public int size() {
-    //
-//	}
+    public int size() {
+    return size;
+	}
 
 
      /** 
@@ -45,13 +46,14 @@ public class List_inArraySlots {
       Double the capacity of the List_inArraySlots, 
       preserving existing data
      */
-     // private void expand() {
-        // System.out.println( "expand... (for debugging)");
-           // /* S.O.P. rules for debugging:
+     private void expand() {
+        System.out.println( "expand... (for debugging)");
+        
+   // /* S.O.P. rules for debugging:
               // Working methods should be silent. But during 
               // development, the programmer must verify that 
               // this method is called when that is appropriate.
               // So test using the println(), then comment it out.
               // */
-     // }
+     }
 }
