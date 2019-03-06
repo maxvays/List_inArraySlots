c8dea39d7d69862c04857b3907b18a95eba5111c
Max Vaysburd <max.vays@gmail.com>
2019-03-06 13:13:03 -0500

    Implement expand

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -6,23 +6,23 @@
 public class List_inArraySlots {
 
     // declare fields here
-	int[] list;
-	int size;
+    int[] list;
+    int size;
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
-    list = new int[10];
-	}
+        list = new int[10];
+    }
 
 
     /** 
       @return the number of elements in this list
      */
     public int size() {
-    return size;
-	}
+        return size;
+    }
 
 
      /** 
@@ -54,16 +54,17 @@ public class List_inArraySlots {
      */
      private void expand() {
         System.out.println( "expand... (for debugging)");
-        
+        List_inArraySlots copy = new List_inArraySlots;
+        copy.list = new int[size * 2];
+        for(int i = 0; i < size; i++){
+            copy.list[i] = list[i];
+        }
+        this = copy;
    // /* S.O.P. rules for debugging:
               // Working methods should be silent. But during 
               // development, the programmer must verify that 
               // this method is called when that is appropriate.
               // So test using the println(), then comment it out.
               // */
-<<<<<<< HEAD
-     // }
-=======
      }
->>>>>>> d1db718207a3c0d877928a90cbb0c8585e9a773b
 }
